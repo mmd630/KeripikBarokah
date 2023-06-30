6f715087d512131ab1a934be2ff3238849b163e1
Yehezkiel Stephanus Austin <105846814+YehezkielSaragih@users.noreply.github.com>
2023-06-30 15:15:35 +0700

    Prototype done

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,11 +8,11 @@ function Preview() {
         <Row> 
           <Col className="d-flex justify-content-center p-5" lg={6}>
             <div>
-              <iframe width="560" height="315"  src="https://www.youtube.com/embed/vihWettcK_c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+              <iframe src="https://www.youtube.com/embed/vihWettcK_c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
             </div>
           </Col>
-          <Col className="p-5" lg={6}>
-            <h3>Isi apakek</h3>
+          <Col className="d-flex justify-content-center p-5" lg={6}>
+            <a href="www.youtube.com">Beli Disini</a>
           </Col>
         </Row>
       </Container>
